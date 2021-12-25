e5db3c1d95d33c611cdf5c57ffd017ebc6415c02
Satish Prasad <spd61185@gmail.com>
2021-12-26 04:55:21 +0530

    NotificationService With Open Closed Principles

---
+++ solid-principle-lab/src/com/satishlabs/ocp/MobileNotificationService.java
@@ -0,0 +1,19 @@
+package com.satishlabs.ocp;
+
+public class MobileNotificationService implements NotificationService{
+
+	@Override
+	public void sendOTP(String medium) {
+		//write logic to send OPT to mobile
+		//use twillo api
+
+	}
+
+	@Override
+	public void sendTransactionReport(String medium) {
+		//write logic to send OPT to mobile
+		//use twillo api
+
+	}
+
+}
---
+++ solid-principle-lab/src/com/satishlabs/ocp/WhatsAppNotificationService.java
@@ -0,0 +1,17 @@
+package com.satishlabs.ocp;
+
+public class WhatsAppNotificationService implements NotificationService{
+
+	@Override
+	public void sendOTP(String medium) {
+		//logic to integrate whatsApp api
+		
+	}
+
+	@Override
+	public void sendTransactionReport(String medium) {
+		//logic to integrate whatsApp api
+		
+	}
+
+}
