ee509efee6a8a859b2834db8bd88ac006fcc4949
Satish Prasad <spd61185@gmail.com>
2021-12-25 18:26:01 +0530

    LoanService Single Responsibility

---
+++ solid-principle-lab/src/com/satishlabs/srp/LoanService.java
@@ -0,0 +1,15 @@
+package com.satishlabs.srp;
+
+public class LoanService {
+	public void getLoanIntersetInfo(String loanType) {
+		if(loanType.equals("homeLoan")) {
+			//do some job
+		}
+		if(loanType.equals("personalLoan")) {
+			//do some job
+		}
+		if(loanType.equals("carLoan")) {
+			//do some job
+		}
+	}
+}
