a67a8e0698405a0ba020056a6dd08ef3a7899486
Satish Prasad <spd61185@gmail.com>
2021-12-25 18:25:14 +0530

    BankService Single Responsibility

--- solid-principle-lab/src/com/satishlabs/srp/BankService.java
+++ solid-principle-lab/src/com/satishlabs/srp/BankService.java
@@ -1,30 +1,14 @@
 package com.satishlabs.srp;
 
 public class BankService {
-	public long deposit(long amount,String accountNo) {
-		//deposit amount
-		return 0;
-	}
-	
-	public long withDraw(long amount,String accountNo) {
-		//withdraw amount
-		return 0;
-				
-	}
-	
-	public void printPassbook() {
-		//update transction info in passbook
-	}
-	
-	public void getLoanInterestInfo(String loanType) {
-		if(loanType.equals("homeLoan")) {
-			//do some job
-		}
-		if(loanType.equals("personalLoan")) {
-			//do some job
-		}
-		if(loanType.equals("carLoan")) {
-			//do some job
-		}
-	}
+	 public long deposit(long amount, String accountNo) {
+	        //deposit amount
+	        return 0;
+	    }
+
+	    public long withDraw(long amount, String accountNo) {
+	        //withdraw amount
+	        return 0;
+	    }
+
 }
