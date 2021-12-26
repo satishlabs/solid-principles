dd520a1c8dcedd8b71c2f8f622f7c0cc68b8f29f
Satish Prasad <spd61185@gmail.com>
2021-12-26 09:26:51 +0530

    SocialMedia With Interface Segaration Principle

---
+++ solid-principle-lab/src/com/satishlabs/isp/Test.java
@@ -0,0 +1,9 @@
+package com.satishlabs.isp;
+
+public class Test {
+/*
+ The principle state that the larger interfaces split into smaller onces.
+ Because the implementation classes use only the methods that are required .
+ We should not force the client to use the methods that they do not want to use.
+ * */
+}
---
+++ solid-principle-lab/src/com/satishlabs/isp/UPIPayments.java
@@ -0,0 +1,8 @@
+package com.satishlabs.isp;
+
+public interface UPIPayments {
+	public void payMoney();
+	public void getScratchCard();
+	public void getCashBackAsCreditBalance();
+	
+}
