51559c25747252dd53572c010e630978a7a22812
Satish Prasad <spd61185@gmail.com>
2021-12-26 09:28:24 +0530

    UPIPayment With Interface Segaration Principle

--- solid-principle-lab/src/com/satishlabs/isp/UPIPayments.java
+++ solid-principle-lab/src/com/satishlabs/isp/UPIPayments.java
@@ -1,5 +1,10 @@
 package com.satishlabs.isp;
 
+/*
+The principle state that the larger interfaces split into smaller onces.
+Because the implementation classes use only the methods that are required .
+We should not force the client to use the methods that they do not want to use.
+* */
 public interface UPIPayments {
 	public void payMoney();
 	public void getScratchCard();
