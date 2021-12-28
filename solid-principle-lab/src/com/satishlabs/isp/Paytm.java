87c7d95a5cf746551a287ae65d86a61899d7a483
Satish Prasad <spd61185@gmail.com>
2021-12-28 11:51:38 +0530

    UPIPayment With Interface Segaration Principle

---
+++ solid-principle-lab/src/com/satishlabs/isp/Paytm.java
@@ -0,0 +1,23 @@
+package com.satishlabs.isp;
+
+public class Paytm implements UPIPayments{
+
+	@Override
+	public void payMoney() {
+		//this fetaures is there in paytm
+		
+	}
+
+	@Override
+	public void getScratchCard() {
+		//this fetaures is there in paytm
+		
+	}
+
+	@Override
+	public void getCashBackAsCreditBalance() {
+		//Not applicable, but it force to implements
+		
+	}
+
+}
---
+++ solid-principle-lab/src/com/satishlabs/isp/Phonepe.java
@@ -0,0 +1,23 @@
+package com.satishlabs.isp;
+
+public class Phonepe implements UPIPayments{
+
+	@Override
+	public void payMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getScratchCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCashBackAsCreditBalance() {
+		//Not applicable, but it inforce to implement
+		
+	}
+
+}
